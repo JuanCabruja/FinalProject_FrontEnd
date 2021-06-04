3d805c45ef4e6cf781f5cfe79c36f4893f73be1a
Juan Francisco José Cabruja <juamcabrujav@MacBook-Pro-de-Juan.local>
2021-06-04 17:44:10 +0200

    Aplicación REACT limpia para desarrollo

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="row justify-content-center" >
+      <h1>Esta sí es la primera línea del proyecto.</h1>
+      <h2>Establece el organigrama, ya tienes mongo controlado</h2>
+      <h2>Recuerda que tienes los PDFs con todo lo necesario</h2>
+    </div>
+    </>
+  );
+}
+
+export default App;
